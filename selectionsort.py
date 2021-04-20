804f09d6a61e1dbbdd3f71da93f22a776634a1f6
Jeremiah <70296474+jeremiah906@users.noreply.github.com>
2021-04-20 12:31:06 -0400

    Create selectionsort.py

---
+++ selectionsort.py
@@ -0,0 +1,12 @@
+def selection_sort(L):
+
+    for i in range(len(L)-1):
+        
+        min_index = i
+ 
+        for j in range(i+1, len(L)-1):
+            
+            if L[j] < L[min_index]:
+                min_index = j
+    
+        L[i], L[min_index] = L[min_index], L[i]
